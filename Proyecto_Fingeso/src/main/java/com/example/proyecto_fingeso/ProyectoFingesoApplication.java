991ec129a6d6334dcf50c4b1eb1d43d8818363a4
Sara Salas <sara.salas@usach.cl>
2024-05-15 22:23:00 -0400

    Creación base proyecto
    Se agregaron las clases identificadas hasta el momento (sin contenido) además de agregar los packpage correspondientes

---
+++ Proyecto_Fingeso/src/main/java/com/example/proyecto_fingeso/ProyectoFingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_fingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFingesoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFingesoApplication.class, args);
+    }
+
+}
---
+++ Proyecto_Fingeso/src/test/java/com/example/proyecto_fingeso/ProyectoFingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_fingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFingesoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
